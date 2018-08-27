fcdedb850d96b620e4c2f32d0bf78a966505e8fd
Ivan S <i.shiryaev@protonmail.com>
2018-08-27 20:25:58 +0700

    Another change to aces behavior

--- lib/hand.rb
+++ lib/hand.rb
@@ -12,10 +12,6 @@ class Hand
     @cards << card
     @score += card.value
     @aces_amount += 1 if is_ace?(card.rank)
-    if starting_aces? && aces_amount > 1
-      @score -= 10
-      return false
-    end
     change_aces_value if score > 21 && aces_amount > 0
   end
 
@@ -31,18 +27,16 @@ class Hand
 
   attr_reader :closed_hand, :aces_amount
 
-  def change_aces_value
-    aces_amount.times do
-      @score -= 10
-      @aces_amount -= 1
-    end
-  end
-
   def is_ace?(rank)
     rank == 'a'
   end
 
-  def starting_aces?
-    aces_amount == cards.size
+  def change_aces_value
+    aces_amount.times do
+      if score > 21
+        @score -= 10
+        @aces_amount -= 1
+      end
+    end
   end
 end
